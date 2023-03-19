55b3b4bfea1461eeb4fe6639a34ace759b3d8a3c
iParthDubey <par0765dub@gmail.com>
2023-03-19 18:37:27 +0530

    Spring MicroService Project

---
+++ user/src/main/java/com/abc/user/service/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,14 @@
+package com.abc.user.service.exceptions;
+
+public class ResourceNotFoundException extends  RuntimeException{
+
+    public ResourceNotFoundException(){
+        super("Resource not found on server !!");
+    }
+
+    public  ResourceNotFoundException(String message){
+        super(message);
+    }
+
+}
+
